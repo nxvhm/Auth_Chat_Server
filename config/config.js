0b6404cfb2a8fb7f8f279a141e64ed765e80db35
Antoan Partadzhiev <a.partadzhiev@nurun.bg>
2018-10-22 14:10:12 +0300

    Extract bcrypt rounds value to config

--- config/config.js
+++ config/config.js
@@ -3,8 +3,14 @@ let env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
     process.env.PORT = 3900;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/users';
+    process.env.BCRYPT_AUTH_ROUNDS = 11;
   } else if (env === 'test') {
     process.env.PORT = 5555;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/usersTest';
+    process.env.BCRYPT_AUTH_ROUNDS = 11;
+  } else if (env == 'prod' || env == 'production') {
+    process.env.PORT = 5555;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/usersTest';
+    process.env.BCRYPT_AUTH_ROUNDS = 12;
   }
   
