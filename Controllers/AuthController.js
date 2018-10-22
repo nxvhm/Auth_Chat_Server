7b649ef44fe2f36bfac67e564014734d3a20a137
Antoan Partadzhiev <a.partadzhiev@nurun.bg>
2018-10-22 17:50:01 +0300

    Validate user signup data synchronous @ AuthController@signup

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,20 +1,24 @@
 const User = require('./../models/user');
 
 module.exports = {
+
   // Signup Account
   signup: function(request, response) {
-    let user = new User(request.body);
-    let result = {};
     
-    user.save().then( user=> {
-      result = {success: 1, error: 0, msg: 'User created'};
-      response.send(result);
-    }, err => {
-      result = {success: 0, error: 1, msg: err}
-      response.send(result);
-    });
+    request.body.password = "";
+    let user = new User(request.body);
+
+    let errors = user.validateSync();
+
+    if (errors) {
+      return response.send(errors);
+    }
+    
+    let result = user.save();
+
+    response.send(result);
   },
-  
+
   // Login Account to Application
   login: function(request, response) {
     let params = req.body;
