16aa29124623143e6a1923156fcc47761ea6a3b1
Antoan Partadzhiev <a.partadzhiev@nurun.bg>
2018-10-22 14:08:56 +0300

    Use logic from controllers folder for /login,/signup routes @ server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const {User}       = require('./models/user');
+const User       = require('./models/user');
+const Controllers = require('./Controllers/Controllers');
 
 console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
@@ -14,26 +15,11 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 
-app.post('/login', (req, res) => {
-    let params = req.body;
-    let user = new User(params);
-
-    user.save().then((user) => {
-        
-        console.log('User created', user);
-        
-    }, err => {
-        console.log('Error occured while trying to save User in db', err);
-    });
-
-    res.send(params);
-}, (error) => {
+app.post('/login', Controllers.AuthController.login, (error) => {
     res.status(400).send(error);
 });
 
-app.get('/signup', (req, res) => {
-    res.send(req);
-}, (error) => {
+app.post('/signup', Controllers.AuthController.signup, (error) => {
     res.status(400).send(error);
 });
 
