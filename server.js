d0b86c3e42141488df6077c8cb1a288039ac2c4c
unknown <a.partadzhiev@gmail.com>
2019-03-07 16:29:51 +0200

    GET /avatars returns list of available avatars

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
     // Configure with .env file for every env diff. then prod.
     require('dotenv').config();
 } else {
-    // Use config file 
+    // Use config file
     require('./config/config');
 }
 
@@ -12,6 +12,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const {AuthController} = require('./Controllers/Controllers');
+const fs = require('fs');
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 console.log(process.env.MONGODB_URI);
@@ -20,14 +21,14 @@ var app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 app.use(bodyParser.json());
-
+app.use(express.static('public'));
 
 app.post('/login', AuthController.login, (error) => {
     res.status(400).send(error);
 });
 
 app.post('/signup', AuthController.signup, (error) => {
-    res.status(400).send(error);    
+    res.status(400).send(error);
 });
 
 app.get('/users/active', (req, res) => {
@@ -52,17 +53,31 @@ app.get('/users/active', (req, res) => {
         {name: 'Lindsay', 'avatar': '/images/avatar/small/lindsay.png'},
         {name: 'Matthew2', 'avatar': '/images/avatar/small/matthew.png'},
         {name: 'Jenny Hess2', 'avatar': '/images/avatar/small/jenny.jpg'},
-        {name: 'Veronika Ossi2', 'avatar': '/images/avatar/small/veronika.jpg'}                        
-      ];  
+        {name: 'Veronika Ossi2', 'avatar': '/images/avatar/small/veronika.jpg'}
+      ];
 
     res.send(userList)
-}, (error) => {
-    res.status(400).send(error);
+}, err => {
+    res.status(400).send(err);
 });
+
+app.get('/avatars', (req, res) => {
+  let avatarsFolder = './public/avatars';
+  let avatars = [];
+
+  fs.readdirSync(avatarsFolder).forEach(file => {
+    let avatarUrl = req.protocol + '://' + req.get('host') + req.originalUrl + file;
+    avatars.push(avatarUrl);
+  });
+
+  res.send(avatars);
+}, err => {
+  res.status(400).send(err);
+})
 
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
-  
+
 module.exports = {app};
