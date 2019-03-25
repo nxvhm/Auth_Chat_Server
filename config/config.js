d2b505caf275d77781e3cabeff69f82b7c4f2a94
unknown <a.partadzhiev@gmail.com>
2019-03-25 13:58:46 +0200

    update example config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 let env = process.env.NODE_ENV || 'development';
 
-process.env.PORT = 5555;
-process.env.MONGODB_URI = 'mongodb://localhost:27017/usersTest';
+process.env.PORT = 3900;
+process.env.MONGODB_URI = 'mongodb://localhost:27017/chatty';
 process.env.BCRYPT_AUTH_ROUNDS = 12;
 process.env.JWT_PRIVATE_KEY = '5834utfde&%$dasdas';
-  
+process.env.APP_URL='http://localhost:3900/'
